1cf744fe5c5181479364f690868713c7cc267730
mrjkc <juliuskvedaras@yahoo.ie>
2017-06-23 20:10:54 +0000

    first commit

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,4 +1,7 @@
 class HangpersonGame
+  
+  attr_accessor :word, :guesses, :wrong_guesses
+
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
@@ -10,6 +13,8 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   def self.get_random_word
@@ -17,6 +22,14 @@ class HangpersonGame
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
+  end
+  
+  def guess(char)
+      if @word.include? char
+        @guesses = char
+      else
+        @wrong_guesses = char
+      end
   end
 
 end
